c07045b9365461a44be3cdfcc36f01f4c4402528
Kim Grönqvist <kg@conrab.se>
2018-07-15 01:18:26 +0200

    Initial changes.

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 4,
+    jsxBracketSameLine: true,
+    overrides: [
+        {
+            files: "*.css",
+            options: {
+                singleQuote: false
+            }
+        }
+    ]
+};
