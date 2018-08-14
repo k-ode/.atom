bb56d4700a148c0f8c6abad27735f88c7254d9ce
Kim Grönqvist <kimgronqvist@gmail.com>
2018-08-14 09:57:21 +0200

    More changes.

--- init.js
+++ init.js
@@ -101,15 +101,11 @@ atom.keymaps.add(__filename, {
         'ctrl-a': 'editor:move-to-first-character-of-line',
         'ctrl-e': 'editor:move-to-end-of-line',
         'ctrl-up': 'editor:move-to-beginning-of-previous-paragraph',
-        'ctrl-down': 'editor:move-to-beginning-of-next-paragraph',
-        'ctrl-shift-up': 'editor:select-to-beginning-of-previous-paragraph',
-        'ctrl-shift-down': 'editor:select-to-beginning-of-next-paragraph'
+        'ctrl-down': 'editor:move-to-beginning-of-next-paragraph'
     },
     'atom-workspace atom-text-editor': {
         'ctrl-left': 'editor:emacs-move-subword-left',
         'ctrl-right': 'editor:emacs-move-subword-right',
-        'ctrl-shift-left': 'editor:emacs-select-subword-left',
-        'ctrl-shift-right': 'editor:emacs-select-subword-right',
         'ctrl-backspace': 'editor:delete-to-beginning-of-subword',
         'ctrl-delete': 'editor:delete-to-end-of-subword',
         'alt-up': 'editor:move-line-up',
@@ -134,13 +130,28 @@ atom.keymaps.add(__filename, {
         'ctrl-x t': 'tree-view:toggle',
         'ctrl-c s s': 'project-find:toggle',
         'ctrl--': 'core:undo',
-        'alt--': 'core:redo'
+        'alt--': 'core:redo',
+        'ctrl-c p p': 'application:reopen-project',
+        'ctrl-x i': 'symbols-view:toggle-file-symbols'
+    },
+    'atom-workspace atom-text-editor.editor': {
+        'ctrl-space': 'selection-mode:toggle'
+    },
+    'atom-workspace atom-text-editor.editor.selection-mode': {
+        'ctrl-g': 'selection-mode:off'
     }
 });
 
 //
 //
 // Packages
+usePackage('last-cursor-position', {
+    keymap: {
+        'atom-workspace': {
+            'ctrl-u ctrl-space': 'last-cursor-position:previous'
+        }
+    }
+})
 
 usePackage('disable-keybindings', {
     config: {
@@ -151,14 +162,155 @@ usePackage('disable-keybindings', {
     }
 });
 
-// usePackage("base16-syntax");
-// usePackage('file-icons');
+usePackage('file-icons');
 
-// usePackage("monokai", {
-//   init: () => {
-//     atom.config.set("core.themes", ["atom-dark-ui", "monokai"]);
-//   }
-// });
+usePackage('emacs-plus', {
+    keymap: {
+        body: {
+            'alt-v': 'core:page-up',
+            'alt-w': 'core:copy',
+            'ctrl-d': 'core:delete',
+            'ctrl-g': 'core:cancel',
+            'ctrl-h': 'core:backspace',
+            'ctrl-m': 'core:confirm',
+            'ctrl-n': 'core:move-down',
+            'ctrl-p': 'core:move-up',
+            'ctrl-v': 'core:page-down',
+            'ctrl-w': 'core:cut'
+        },
+        'atom-workspace': {
+            'alt-x': 'command-palette:toggle',
+            'ctrl-x 0': 'pane:close',
+            'ctrl-x 1': 'emacs-plus:close-other-panes',
+            'ctrl-x 2': 'pane:split-down-and-copy-active-item',
+            'ctrl-x 3': 'pane:split-right-and-copy-active-item',
+            'ctrl-x b': 'fuzzy-finder:toggle-buffer-finder',
+            'ctrl-x ctrl-c': 'application:quit',
+            'ctrl-x ctrl-f': 'advanced-open-file:toggle',
+            'ctrl-x ctrl-j': 'tree-view:reveal-active-file',
+            'ctrl-x k': 'core:close',
+            'ctrl-x o': 'window:focus-next-pane'
+        },
+        'atom-workspace atom-text-editor': {
+            'ctrl-/': 'core:undo',
+            'ctrl-_': 'core:undo',
+            'ctrl-a': 'editor:move-to-beginning-of-line',
+            'ctrl-b': 'core:move-left',
+            'ctrl-e': 'editor:move-to-end-of-screen-line',
+            'ctrl-f': 'core:move-right',
+            'ctrl-g': 'editor:consolidate-selections',
+            'ctrl-t': 'editor:transpose',
+            'ctrl-y': 'core:paste',
+            'alt-b': 'editor:move-to-beginning-of-word',
+            'alt-f': 'editor:move-to-end-of-word',
+            'alt-right': 'editor:move-to-end-of-word',
+            'ctrl-alt-/': 'core:redo',
+            'ctrl-x ctrl-l': 'editor:lower-case',
+            'ctrl-x ctrl-u': 'editor:upper-case',
+            'ctrl-x u': 'core:undo'
+        },
+        'atom-workspace atom-text-editor.emacs-plus:not([mini])': {
+            'alt-.': 'symbols-view:toggle-file-symbols',
+            'alt-/': 'autocomplete-plus:activate',
+            'alt-;': 'editor:toggle-line-comments',
+            'alt-<': 'core:move-to-top',
+            'alt->': 'core:move-to-bottom',
+            'alt-\\': 'emacs-plus:delete-horizontal-space',
+            'alt-^': 'emacs-plus:delete-indentation',
+            'alt-backspace': 'emacs-plus:backward-kill-word',
+            'alt-c': 'emacs-plus:capitalize-word',
+            'alt-d': 'emacs-plus:kill-word',
+            'alt-delete': 'emacs-plus:backward-kill-word',
+            'alt-g g': 'go-to-line:toggle',
+            'alt-left': 'editor:move-to-beginning-of-word',
+            'alt-m': 'editor:move-to-first-character-of-line',
+            'alt-q': 'autoflow:reflow-selection',
+            'alt-space': 'emacs-plus:just-one-space',
+            'alt-t': 'emacs-plus:transpose-words',
+            'alt-w': 'emacs-plus:copy',
+            'alt-{': 'editor:move-to-beginning-of-previous-paragraph',
+            'alt-}': 'editor:move-to-beginning-of-next-paragraph',
+            'ctrl-alt-w': 'emacs-plus:append-next-kill',
+            'ctrl-i': 'editor:auto-indent',
+            'ctrl-j': 'editor:newline',
+            'ctrl-k': 'emacs-plus:kill-line',
+            'ctrl-l': 'emacs-plus:recenter-top-bottom',
+            'ctrl-m': 'editor:newline',
+            'ctrl-o': 'emacs-plus:open-line',
+            'ctrl-r': 'find-and-replace:show',
+            'ctrl-s': 'find-and-replace:show',
+            'ctrl-shift-backspace': 'emacs-plus:kill-whole-line',
+            // 'ctrl-space': 'emacs-plus:set-mark',
+            'ctrl-w': 'emacs-plus:kill-region',
+            'ctrl-x ctrl-s': 'core:save',
+            'ctrl-x ctrl-t': 'emacs-plus:transpose-lines',
+            // 'ctrl-x ctrl-x': 'emacs-plus:exchange-point-and-mark',
+            'ctrl-x h': 'core:select-all'
+        },
+        'atom-workspace atom-text-editor.emacs-plus.mark-mode:not([mini])': {
+            'alt-<': 'core:select-to-top',
+            'alt->': 'core:select-to-bottom',
+            'alt-b': 'editor:select-to-beginning-of-word',
+            'alt-f': 'editor:select-to-end-of-word',
+            'alt-left': 'editor:select-to-beginning-of-word',
+            'alt-right': 'editor:select-to-end-of-word',
+            'alt-v': 'core:select-page-up',
+            'alt-{': 'editor:select-to-beginning-of-previous-paragraph',
+            'alt-}': 'editor:select-to-beginning-of-next-paragraph',
+            'ctrl-a': 'editor:select-to-beginning-of-line',
+            'ctrl-b': 'core:select-left',
+            'ctrl-e': 'editor:select-to-end-of-line',
+            'ctrl-f': 'core:select-right',
+            'ctrl-g': 'core:cancel',
+            'ctrl-n': 'core:select-down',
+            'ctrl-p': 'core:select-up',
+            'ctrl-v': 'core:select-page-down',
+            down: 'core:select-down',
+            left: 'core:select-left',
+            right: 'core:select-right',
+            up: 'core:select-up'
+        },
+        '.find-and-replace': {
+            'ctrl-r': 'find-and-replace:find-previous',
+            'ctrl-s': 'find-and-replace:find-next'
+        },
+        'atom-workspace atom-text-editor:not(mini).emacs-plus.autocomplete-active': {
+            'ctrl-g': 'autocomplete-plus:cancel',
+            'ctrl-m': 'autocomplete-plus:confirm',
+            enter: 'autocomplete-plus:confirm'
+        },
+        '.tree-view': {
+            '^': 'tree-view:collapse-directory',
+            'ctrl-m': 'tree-view:open-selected-entry',
+            n: 'core:move-down',
+            p: 'core:move-up',
+            q: 'tree-view:toggle',
+            'shift-c': 'tree-view:duplicate',
+            'shift-d': 'tree-view:remove',
+            'shift-e': 'tree-view:recursive-expand-directory',
+            'shift-r': 'tree-view:rename'
+        },
+        '.advanced-open-file atom-text-editor': {
+            'ctrl-i': 'advanced-open-file:autocomplete',
+            'ctrl-p': 'advanced-open-file:move-cursor-up',
+            'ctrl-n': 'advanced-open-file:move-cursor-down',
+            'ctrl-w': 'advanced-open-file:delete-path-component',
+            'ctrl-/': 'advanced-open-file:undo'
+        }
+    }
+});
+
+usePackage('emacs-tab', {
+    enableKeys: true
+});
+
+usePackage('join-lines-smarter', {
+    keymap: {
+        'atom-workspace atom-text-editor': {
+            'alt-j': 'join-lines-smarter:join-lines'
+        }
+    }
+});
 
 usePackage('advanced-open-file', {
     config: {
@@ -182,20 +334,8 @@ usePackage('advanced-open-file', {
     }
 });
 
-usePackage('emacs-plus', {
-    enableKeys: true
-});
-
-usePackage('emacs-tab', {
-    enableKeys: true
-});
-
-// usePackage("organized", {
-//   enableKeys: true,
-//   config: {
-//     enableToolbarSupport: false
-//   }
-// });
+// Have to be manually installed for some reason.
+// usePackage('selection-mode');
 
 usePackage('incsearch', {
     keymap: {
@@ -265,9 +405,8 @@ usePackage('autocomplete-plus', {
         }
     },
     config: {
-        autoActivationDelay: 1000,
-        backspaceTriggersAutocomplete: true,
-        confirmCompletion: 'tab always, enter when suggestion explicitly selected',
+        enableAutoActivation: false,
+        confirmCompletion: 'tab and enter',
         enableAutoConfirmSingleSuggestion: false,
         enableExtendedUnicodeSupport: true
     }
@@ -295,19 +434,8 @@ usePackage('github', {
 
 // usePackage("trailing-spaces");
 // usePackage("spaces-in-braces");
-// usePackage("atom-oss-license");
-usePackage('undo-tree');
 
-// usePackage("project-jump", {
-//   keymap: {
-//     "atom-workspace": {
-//       "ctrl-x p a": "project-jump:add",
-//       "ctrl-x p o": "project-jump:open",
-//       "ctrl-x p p": "project-jump:switch",
-//       "ctrl-x p r": "project-jump:remove"
-//     }
-//   }
-// });
+usePackage('undo-tree');
 
 // usePackage("file-watcher", {
 //   config: {
@@ -317,14 +445,6 @@ usePackage('undo-tree');
 
 // usePackage("docblockr", {
 //   enableKeys: true
-// });
-
-// usePackage("atom-beautify", {
-//   keymap: {
-//     "atom-workspace atom-text-editor": {
-//       "ctrl-c ctrl-f": "beautify-editor"
-//     }
-//   }
 // });
 
 //
@@ -371,13 +491,14 @@ usePackage('atom-ide-ui', {
             'ctrl-c ctrl-f': 'code-format:format-code'
         },
         'atom-workspace atom-text-editor.emacs-plus:not([mini])': {
+            'alt-,': 'signature-help:show',
+            'ctrl-alt-,': 'datatip:toggle',
+            'alt-.': 'hyperclick:confirm-cursor'
             // "alt-n": "diagnostics:go-to-next-diagnostic",
             // "alt-p": "diagnostics:go-to-previous-diagnostic",
             // "ctrl-c s": "diagnostics:show-actions-at-position",
             // "ctrl-c alt-s": "diagnostics:fix-all-in-current-file",
             // "ctrl-c r": "find-references:activate",
-            // "ctrl-t": "datatip:toggle",
-            // "alt-.": "hyperclick:confirm-cursor"
         },
         'atom-workspace': {
             // "ctrl-c t": "outline-view:toggle",
@@ -403,36 +524,15 @@ usePackage('ide-typescript', {
 
 //
 //
-// Rust
-//
-// configSet(
-//   "editor",
-//   {
-//     preferredLineLength: 80
-//   },
-//   {scopeSelector: ".rust.source"}
-// );
-//
-// usePackage("ide-rust", {
-//   config: {
-//     rlsToolchain: "nightly",
-//     rlsDefaultConfig: {
-//       allTargets: "On",
-//       clippyPreference: "On"
-//     }
-//   }
-// });
-//
-// usePackage("build-cargo");
-//
-// usePackage("autocomplete-crates");
-// // usePackage("rustsym");
-
-//
-//
 // JavaScript
 
 usePackage('language-babel');
+
+usePackage('linter', {
+    config: {
+        lintOnChange: false
+    }
+});
 
 usePackage('linter-eslint', {
     keymap: {
@@ -455,18 +555,6 @@ usePackage('prettier-atom', {
 
 //
 //
-// PureScript
-
-// usePackage("language-purescript");
-//
-// usePackage("ide-purescript", {
-//   enableKeys: true
-// });
-
-//
-//
 // Misc languages
 
 usePackage('linter-jsonlint');
-// usePackage("language-papyrus");
-// usePackage("language-stellaris");
